1c02b186461ba6e26a5e35b37fcf40a413c9d8a6
Aleksandr Kasianenko <saix61@yandex.ru>
2024-04-28 20:47:12 +0300

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import App from '@Components/App/App'
 import './index.scss'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
